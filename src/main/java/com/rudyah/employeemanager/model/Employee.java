4ac53ac52192cf95f471b3e2e69a37b4cac2d080
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-02-08 16:21:28 -0800

    Implement model/domain and backend connection

---
+++ src/main/java/com/rudyah/employeemanager/model/Employee.java
@@ -0,0 +1,35 @@
+package com.rudyah.employeemanager.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    @NonNull
+    private String jobTitle;
+    @NonNull
+    private String phone;
+    @NonNull
+    private String imageUrl;
+
+    @Column(nullable = false, updatable = false)
+    @NonNull
+    private String employeeId;
+}
